80ac403b544c4b7fc9dd2811c6dac91ce2aa7294
annafu1 <104175541+annafu1@users.noreply.github.com>
2022-10-29 14:17:17 -0400

    I have added trig methods, switch cases, and tested them.

--- src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
@@ -18,7 +18,7 @@ public class Console {
     public static int getIntegerInput(String prompt) {
         Scanner scanner = new Scanner(System.in);
         println(prompt);
-        int  userInput = scanner.nextInt();
+        int userInput = scanner.nextInt();
         return userInput;
     }
 
@@ -32,7 +32,7 @@ public class Console {
     public static Double getDoubleInput(String prompt) {
         Scanner scanner = new Scanner(System.in);
         println(prompt);
-        double  userInput = scanner.nextDouble();
+        double userInput = scanner.nextDouble();
         return userInput;
 
     }
@@ -48,6 +48,7 @@ public class Console {
 
 
     }
+
     public static double multiplication(double i, double j) {
 
         return i * j;
@@ -57,23 +58,43 @@ public class Console {
 
         return i / j;
     }
+
     public static double square(double i) {
 
         return i * i;
     }
+
     public static double squareRoot(double i) {
 
         return Math.sqrt(i);
     }
+
     public static double exponents(double i, double j) {
 
-        return Math.pow(i,j);
+        return Math.pow(i, j);
     }
+
     public static double inverse(double i) {
-        String sNumber = (String) String.format("%.2f", (1 /i));
+        String sNumber = (String) String.format("%.2f", (1 / i));
         Double newNumber = Double.parseDouble(sNumber);
-        return (1 / i);
+        return (newNumber);
     }
 
+    public static double sin(double i) {
+        String sNumber1 = (String) String.format("%.2f", (Math.sin(i)));
+        Double newSin = Double.parseDouble(sNumber1);
+        return (newSin);
+    }
 
+    public static double cos(double i) {
+        String sNumber2 = (String) String.format("%.2f", (Math.cos(i)));
+        Double newCos = Double.parseDouble(sNumber2);
+        return (newCos);
+    }
+
+    public static double tan(double i) {
+        String sNumber3 = (String) String.format("%.2f", (Math.tan(i)));
+        Double newTan = Double.parseDouble(sNumber3);
+        return (newTan);
+    }
 }
